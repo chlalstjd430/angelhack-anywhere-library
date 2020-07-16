06114cf48bc7af2abfef3a6f1d454176767650ca
chlalstjd430 <cms3136@gmail.com>
2020-07-16 22:04:03 +0900

    complement sign-up api

---
+++ anywhere-library-application/src/main/java/com/anglehack/anywhereLibrary/api/user/response/CheckDuplicateResponse.java
@@ -0,0 +1,10 @@
+package com.anglehack.anywhereLibrary.api.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CheckDuplicateResponse {
+    private String messasge;
+}
