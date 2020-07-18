497cff917a2b5f6b141ac3605613d031c0ca364d
MinSeong <cms3136@gmail.com>
2020-07-19 01:27:30 +0900

    implement leave seat api (#28)

---
+++ anywhere-library-domain-rds/src/main/java/com/anglehack/anywhereLibrary/api/user/repository/UserLearningTimeRepository.java
@@ -0,0 +1,9 @@
+package com.anglehack.anywhereLibrary.api.user.repository;
+
+import com.anglehack.anywhereLibrary.api.common.repository.ExtendRepository;
+import com.anglehack.anywhereLibrary.api.user.entity.UserLearningTime;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserLearningTimeRepository extends ExtendRepository<UserLearningTime> {
+}
